75e6802a8cba748d0dde67b4566bdf12d1b16543
craftcsc <3012915270101@ingenieria.usac.edu.gt>
2023-10-05 21:16:48 -0600

    ya jalo jaja

--- BackEnd/app.ts
+++ BackEnd/app.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import UserRoutes from './routes/UserRoutes';
-import logRequest from './utils/logRequest';
 import { port } from './constants';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(logRequest);
 
 app.use('/user', UserRoutes);
 
