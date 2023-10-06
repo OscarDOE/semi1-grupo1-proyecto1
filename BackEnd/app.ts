3a43375b893a970a51ed26e8ce7e9cd61af675ec
craftcsc <3012915270101@ingenieria.usac.edu.gt>
2023-10-06 12:46:26 -0600

    s3

--- BackEnd/app.ts
+++ BackEnd/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import UserRoutes from './routes/UserRoutes';
 import { port } from './constants';
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/user', UserRoutes);
 
--- BackEnd/routes/UserRoutes.ts
+++ BackEnd/routes/UserRoutes.ts
@@ -1,11 +1,37 @@
-import express from 'express';
+import {express } from 'express';
 import UserController from '../controllers/UserController';
 import authenticateToken from '../middleware/authenticateToken'; 
 
 const UserRoutes = express.Router();
 
+//
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+
+aws.config.update({
+  secretAccessKey: 'AKIA5AQSSJ7MHYABWLE4',
+  accessKeyId: 'uBH1D1y2tEDWxysqFTe0WdmuCXd3G05P10vWNRli',
+  region: 'us-east-2', // Cambia esto a la región de tu bucket
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'arn:aws:s3:::multimedia-semi1-g1',
+    acl: 'public-read', // Puedes ajustar los permisos
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + '-' + file.originalname);
+    },
+  }),
+});
+
+//
+
 UserRoutes.post('/login',   UserController.login);
-UserRoutes.post('/register',  UserController.registerUser);
+UserRoutes.post('/register', upload.single('imagen'), UserController.registerUser);
 UserRoutes.post('/updateuser',authenticateToken, UserController.updateUser);
 UserRoutes.get('/getusers', UserController.getAllUsers);
 
